634a52f81050e41a2e38e3eb20276e6fbce716af
BuildTools <unconfigured@null.spigotmc.org>
2020-10-12 08:50:54 +0700

    Berhasil membuat garis dengan Line Strip

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ function main() {
     var vertices = [
         -0.5, 0.5,  //A
         -0.5, -0.5, //B
-        0.5, -0.5  //C
+        0.5, -0.5,  //C
+        -0.5, 0.5  //A
     ];
 
     //buffer
@@ -49,9 +50,9 @@ function main() {
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    var primitive = gl.LINE_LOOP;
+    var primitive = gl.LINE_STRIP;
     var offset = 0;
-    var count = 3;
+    var count = 4;
     
     gl.drawArrays(primitive, offset, count);
 }
