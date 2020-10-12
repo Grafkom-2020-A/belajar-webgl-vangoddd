2f3cd2ada04c07f65c35e2c1b2c8ed78257f359a
BuildTools <unconfigured@null.spigotmc.org>
2020-10-12 08:47:57 +0700

    Berhasil membuat garis

--- main.js
+++ main.js
@@ -2,22 +2,36 @@ function main() {
     var canvas = document.getElementById("myCanvas");
     var gl = canvas.getContext("webgl");
 
-    var vertexShaderCode = `
-    void main() {
-        gl_Position = vec4(0.0, 0.0, 0.0, 1.0);
-        gl_PointSize = 10.0;
-    }
-    `;
+    //vertex definition
+    /**
+     * A (-0.5, 0.5) 
+     * B (-0.5, -0.5)
+     * C (0.5, -0.5)
+     */
+    var vertices = [
+        -0.5, 0.5,  //A
+        -0.5, -0.5, //B
+        -0.5, -0.5, //B
+        0.5, -0.5,  //C
+        -0.5, 0.5,  //A
+        0.5, -0.5   //C
+    ];
+
+    //buffer
+    var positionBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    //getting vertex shader
+    var vertexShaderCode = document.getElementById("vertexShaderCode").text;
 
     var vertexShader = gl.createShader(gl.VERTEX_SHADER);
     gl.shaderSource(vertexShader, vertexShaderCode);
     gl.compileShader(vertexShader);
 
-    var fragmentShaderCode = `
-    void main() {
-        gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
-    }
-    `;
+    //getting fragment shader
+    var fragmentShaderCode = document.getElementById("fragmentShaderCode").text;
 
     var fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
     gl.shaderSource(fragmentShader, fragmentShaderCode);
@@ -29,8 +43,18 @@ function main() {
     gl.linkProgram(shaderProgram);
     gl.useProgram(shaderProgram);
 
+    //bind
+    gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    var aPosition = gl.getAttribLocation(shaderProgram, "a_Position");
+    gl.vertexAttribPointer(aPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(aPosition);
+
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
-    gl.drawArrays(gl.POINTS, 0, 1);
+    var primitive = gl.LINES;
+    var offset = 0;
+    var count = 6;
+    
+    gl.drawArrays(primitive, offset, count);
 }
